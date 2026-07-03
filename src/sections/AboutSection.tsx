c5dcad149a7d2491baceb2f04d521b0a3f6e33de
CoolBlaze81 <mannansajjan@gmail.com>
2026-07-04 02:17:40 +0530

    about change 2

--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -28,8 +28,8 @@ export default function AboutSection() {
         duration={0.9}
         className="
 absolute
--top-10 md:top-[4%]
--left-14 md:left-[4%]
+-top-2 md:top-[4%]
+-left-8 md:left-[4%]
 w-[110px] md:w-[210px]
 "
       >
@@ -43,8 +43,8 @@ w-[110px] md:w-[210px]
         duration={0.9}
         className="
 absolute
-bottom-0 md:bottom-[8%]
--left-16 md:left-[10%]
+bottom-16 md:bottom-[8%]
+-left-8 md:left-[10%]
 w-[95px] md:w-[180px]
 "
       >
@@ -58,8 +58,8 @@ w-[95px] md:w-[180px]
         duration={0.9}
         className="
 absolute
--top-8 md:top-[4%]
--right-14 md:right-[4%]
+-top-3 md:top-[4%]
+-right-8 md:right-[4%]
 w-[110px] md:w-[210px]
 "
       >
@@ -71,7 +71,7 @@ w-[110px] md:w-[210px]
         x={80}
         y={0}
         duration={0.9}
-        className="absolute bottom-[8%] right-[3%] sm:right-[6%] md:right-[10%] w-[130px] sm:w-[170px] md:w-[220px]"
+        className="absolute bottom-14 right-8 sm:right-[6%] md:right-[10%] w-[130px] sm:w-[170px] md:w-[220px]"
       >
         <img src={GROUP_URL} alt="Decorative 3D group" className="w-full h-auto" />
       </FadeIn>
